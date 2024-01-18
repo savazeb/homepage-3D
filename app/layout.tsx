237a04a045488776a5a5da3bd2c5bf669fd6dcab
savazeb <inincoincognito@gmail.com>
2024-01-18 17:10:41 +0900

    init commit

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+import '@/styles/theme.scss';
+
+const RootLayout = ({ children }: { children: React.ReactNode; }) => {
+  return (
+    <html lang="en" >
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
